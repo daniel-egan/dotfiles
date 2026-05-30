8977725c845c827c90923534071391557567852c
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2026-05-30 12:55:37 +0100

    Add Elan Touchpad quirks configuration to restart script

--- scripts/restart-touchpad.sh
+++ scripts/restart-touchpad.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
+set -e
 
-sudo rmmod i2c_hid_acpi && sudo modprobe i2c_hid_acpi
+QUIRK_FILE=/etc/libinput/local-overrides.quirks
+sudo mkdir -p /etc/libinput
+sudo tee "$QUIRK_FILE" > /dev/null <<'QUIRK_EOF'
+# Elan Touchpad fix - treat as trackpad not touchscreen
+[Elan CUST0000:00]
+MatchBus=i2c
+MatchVendor=04F3
+MatchProduct=2A81
+MatchName=CUST0000:00 04F3:2A81
+AttrPressureRange=0:0
+QUIRK_EOF
+
+sudo rmmod i2c_hid_acpi || true
+sleep 1
+sudo modprobe i2c_hid_acpi
