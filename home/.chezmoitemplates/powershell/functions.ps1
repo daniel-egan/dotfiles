221d537b56418ed7dffe3c3fd3f2f57552c8d7f6
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2026-07-26 14:35:19 +0100

    Added !! function

--- home/.chezmoitemplates/powershell/functions.ps1
+++ home/.chezmoitemplates/powershell/functions.ps1
@@ -16,3 +16,23 @@ function mkcd {
     # Change location to the new directory
     Set-Location -Path $newPath
 }
+
+# !! - Expand to last command (works with "sudo !!" too)
+Set-PSReadLineKeyHandler -Chord "!" -ScriptBlock {
+    param($key, $arg)
+    
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+    
+    # Check if previous character is '!'
+    if ($cursor -gt 0 -and $line[$cursor - 1] -eq '!') {
+        # Delete the previous '!' and insert last command
+        [Microsoft.PowerShell.PSConsoleReadLine]::BackwardDeleteChar()
+        $lastCommand = (Get-History -Count 1).CommandLine
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert($lastCommand)
+    } else {
+        # Just insert a normal '!'
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert('!')
+    }
+}
