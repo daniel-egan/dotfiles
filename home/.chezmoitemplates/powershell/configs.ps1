885efd7588672c439903fe003bcdacf5a1e2e484
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2026-08-18 09:36:41 +0100

    Update PSReadLine options in configs.ps1
    Added option to move history search cursor to end.

--- home/.chezmoitemplates/powershell/configs.ps1
+++ home/.chezmoitemplates/powershell/configs.ps1
@@ -1,3 +1,4 @@
 Set-PSReadLineOption -HistorySavePath (Join-Path $PSScriptRoot ".ps_history")
 Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
 Set-PSReadLineOption -MaximumHistoryCount 10000
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
