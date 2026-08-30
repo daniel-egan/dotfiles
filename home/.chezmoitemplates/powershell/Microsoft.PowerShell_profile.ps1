2aed544a3cd24ead2b5b846f5267e8bb3f53bf1a
daniel-egan <95421705+daniel-egan@users.noreply.github.com>
2026-08-30 09:06:54 +0100

    Update home/.chezmoitemplates/powershell/Microsoft.PowerShell_profile.ps1

--- home/.chezmoitemplates/powershell/Microsoft.PowerShell_profile.ps1
+++ home/.chezmoitemplates/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 . "$PSScriptRoot\env.ps1"
 . "$PSScriptRoot\aliases.ps1"
 . "$PSScriptRoot\functions.ps1"
-. "$PSScriptRoot\init.ps1"
 . "$PSScriptRoot\path.ps1"
+. "$PSScriptRoot\init.ps1"
 . "$PSScriptRoot\configs.ps1"
